8b53cb41a3e73bb82e4d3b3ce7968447f73008e9
Taehwan Noh <taehwanno.dev@gmail.com>
2019-02-07 23:29:35 +0900

    Enable webpack hot reload
    Based on samuelsimoes/chrome-extension-webpack-boilerplate, I enable
    webpack hot reload. content script is excluded in hot reloaded entry.
    
    See:
    - https://github.com/samuelsimoes/chrome-extension-webpack-boilerplate
    - https://github.com/samuelsimoes/chrome-extension-webpack-boilerplate/issues/4#issuecomment-261788690

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,19 @@
-const webpack = require('webpack');
 const path = require('path');
+const { EnvironmentPlugin } = require('webpack');
+const CleanPlugin = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlPlugin = require('html-webpack-plugin');
+const WriteFilePlugin = require('write-file-webpack-plugin');
 
 module.exports = {
+  mode: process.env.NODE_ENV || 'development',
   entry: {
     popup: path.join(__dirname, './src/popup.ts'),
     background: path.join(__dirname, './src/background.ts'),
     'content-script': path.join(__dirname, './src/content-script.ts'),
   },
   output: {
-    path: path.join(__dirname, './dist/js'),
+    path: path.join(__dirname, './dist'),
     filename: '[name].js',
   },
   module: {
@@ -23,4 +28,25 @@ module.exports = {
   resolve: {
     extensions: ['.ts', '.tsx', '.js'],
   },
+  plugins: [
+    new CleanPlugin(['dist']),
+    new EnvironmentPlugin(['NODE_ENV']),
+    new CopyPlugin([{ context: path.join(__dirname, 'public'), from: '**/*' }]),
+    new HtmlPlugin({
+      template: path.join(__dirname, './src/html', 'popup.html'),
+      filename: 'popup.html',
+      chunks: ['popup'],
+    }),
+    new HtmlPlugin({
+      template: path.join(__dirname, './src/html', 'options.html'),
+      filename: 'options.html',
+      chunks: ['options'],
+    }),
+    new HtmlPlugin({
+      template: path.join(__dirname, './src/html', 'background.html'),
+      filename: 'background.html',
+      chunks: ['background'],
+    }),
+    new WriteFilePlugin(),
+  ],
 };
